ec50ef932caaf392688ec621d5567c5378e51506
rutu1518 <rutujamore1508@gmail.com>
2024-07-21 09:16:40 +0530

    initial setup is ready

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const connetDB = async ()=>{
+    const conn = await mongoose.connect(process.env.MONGO_URL)
+    if(conn){
+        console.log(`MongoDB Connected Successfully`);
+    }
+};
+connetDB();
+
+app.get('/', (req, res)=>{
+    
+    res.json({                  
+        message: `Welcome to Expense Tracker API`   
+    })
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+   console.log(`server is running on ${PORT}`);
+})
+
+
+
+
